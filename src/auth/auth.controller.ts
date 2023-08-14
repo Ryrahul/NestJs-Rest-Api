adb7efa69bebea7a69b55a537e88e45a8656151f
Rahul <caulirahul6@gmail.com>
2023-08-15 01:38:06 +0530

    Dto setup and pipes setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post} from "@nestjs/common";
 import { AuthService } from "./auth.services";
+import { AuthDto } from "./dto";
+
 
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService:AuthService){}
-    @Post('signup')
-    signup(){
-        return this.authService.signup()
-    }
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto:AuthDto ) {
+    console.log({ dto });
+    return this.authService.signup();
+  }
+
+
 
     @Post('login')
     login(){
