dbfdc99f886c44fb4f4a8f870aab57a4c2a223fc
Rahul <caulirahul6@gmail.com>
2023-08-14 14:01:49 +0530

    Testing auth routes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,11 @@
 import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.services";
 
 
-@Module({})
+@Module({
+    controllers:[AuthController],
+    providers:[AuthService]
+})
 export class AuthModule{}
 
