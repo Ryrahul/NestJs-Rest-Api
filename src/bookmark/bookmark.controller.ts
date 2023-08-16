e4569bad0b59ac21ed91275f3c71ebd088863043
Rahul <caulirahul6@gmail.com>
2023-08-16 14:15:38 +0530

    update operation for bookmarks

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,17 +1,26 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
 import { BookmarkDto } from './dto/bookmark.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
-
-@Controller('bookmark')
+@Controller('bookmarks')
 export class BookmarkController {
   constructor(private bookmarkservice: BookmarkService) {}
   @UseGuards(AuthGuard)
-  @Post('addBookmark')
+  @Post()
   addBookmark(@Body() dto: BookmarkDto, @Req() request: any) {
     try {
       const UserId = request.user.UserId;
-      console.log(UserId);
       return this.bookmarkservice.addBookmark(UserId, dto);
     } catch (e) {
       console.log(e);
@@ -20,9 +29,31 @@ export class BookmarkController {
     }
   }
   @UseGuards(AuthGuard)
-  @Get('getBookmark')
+  @Get()
   getBookmark(@Req() request: any) {
     const UserId = request.user.UserId;
     return this.bookmarkservice.getBookmark(UserId);
+  }
+  @UseGuards(AuthGuard)
+  @Delete('/:id')
+  deletebookmark(@Req() request: any, @Param('id', ParseIntPipe) id: number) {
+    try {
+      const UserId = request.user.UserId;
+      const BookmarkId = id;
+      return this.bookmarkservice.deletBookmark(UserId, BookmarkId);
+    } catch (e) {
+      return e.message;
+    }
+  }
+  @UseGuards(AuthGuard)
+  @Put('/:id')
+  updatebookmark(
+    @Body() dto: BookmarkDto,
+    @Req() request: any,
+    @Param('id', ParseIntPipe) id: number
+  ) {
+    const UserId = request.user.UserId;
+    const bookmark = id;
+    return this.bookmarkservice.updateBookmark(UserId, bookmark, dto);
   }
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common';
 import { BookmarkDto } from './dto/bookmark.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -19,11 +23,60 @@ export class BookmarkService {
     }
   }
   async getBookmark(UserId: number): Promise<object> {
-    const bookmark = await this.prisma.bookmark.findMany({
-      where: {
-        UserId,
-      },
-    });
-    return bookmark;
+    try {
+      const bookmark = await this.prisma.bookmark.findMany({
+        where: {
+          UserId,
+        },
+      });
+      return bookmark;
+    } catch (e) {
+      return e.message;
+    }
+  }
+  async deletBookmark(UserId: number, bookmarkId: number) {
+    try {
+      const bookmark = await this.prisma.bookmark.findUnique({
+        where: {
+          id: bookmarkId,
+        },
+      });
+      if (!bookmark || bookmark.UserId !== UserId) {
+        throw new ForbiddenException('Permission Denied');
+      }
+      await this.prisma.bookmark.delete({
+        where: {
+          id: bookmarkId,
+        },
+      });
+      return `deleted the bookmark--->> ${bookmark}`;
+    } catch (e) {
+      return e.message;
+    }
+  }
+  async updateBookmark(UserId: number, bookmarkId: number, dto: BookmarkDto) {
+    try {
+      const bookmark = await this.prisma.bookmark.findUnique({
+        where: {
+          id: bookmarkId,
+        },
+      });
+      // console.log(bookmark.UserId)
+      console.log(UserId)
+      if (!bookmark || bookmark.UserId !== UserId) {
+        throw new ForbiddenException('Permission Denied');
+      }
+      return await this.prisma.bookmark.update({
+        where: {
+          id: bookmarkId,
+        },
+        data: {
+          ...dto,
+        },
+      });
+       
+    } catch (e) {
+      return e.message;
+    }
   }
 }
